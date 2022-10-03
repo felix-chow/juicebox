40cd8e72e41af4c6e3e0f5cfd7b436bddbf87818
felix-chow <dong_chow@yahoo.com>
2022-10-02 21:19:53 -0700

    finished part 3

--- db/index.js
+++ db/index.js
@@ -1,7 +1,11 @@
 const { Client } = require('pg');
 
 // supply the db name and location of the database
-const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev');
+const client = new Client({
+    connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev',
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+});
+
 
 // User methods
 
